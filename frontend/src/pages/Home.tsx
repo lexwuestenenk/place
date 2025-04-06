70231134b0091720fb978c562e7b202e9326c1e1
Lex Wuestenenk <lex.wuestenenk@hotmail.com>
2025-04-06 11:52:09 +0200

    Remove unused const

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,9 +1,6 @@
 import { useSelector } from 'react-redux';
 import { RootState } from '../redux/store';
 
-const GRID_WIDTH = 400;
-const GRID_HEIGHT = 200;
-
 export default function Home() {
   const token = useSelector((state: RootState) => state.account.token)
   const user = useSelector((state: RootState) => state.account.user)
