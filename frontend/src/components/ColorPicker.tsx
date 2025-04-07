61ffc6e1449eff0e0a0b21faf3f6b4eb6f324118
Lex Wuestenenk <lex.wuestenenk@hotmail.com>
2025-04-07 23:00:45 +0200

    Fix

--- frontend/src/components/ColorPicker.tsx
+++ frontend/src/components/ColorPicker.tsx
@@ -1,5 +1,6 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Color } from '../types';
+import { CheckIcon, LoaderIcon } from 'lucide-react';
 
 type Props = {
     colors: Color[];
@@ -10,6 +11,7 @@ type Props = {
 };
 
 const ColorPicker: React.FC<Props> = ({ colors, onColorClick, onSubmit, selectedColor, selectedPixel }) => {
+  const [cooldown, setCooldown] = useState<number>(0);
   useEffect(() => {
     const handleKeyDown = (e: KeyboardEvent) => {
       if (e.key === 'Enter') {
@@ -23,12 +25,21 @@ const ColorPicker: React.FC<Props> = ({ colors, onColorClick, onSubmit, selected
 
   return (
     <div className="flex flex-col items-center gap-2 absolute bottom-4 max-w-3/4 left-1/2 -translate-x-1/2">
-      <button
-        onClick={onSubmit}
-        className="px-4 py-2 bg-emerald-500 hover:bg-emerald-600 text-white font-semibold rounded-md shadow transition"
-      >
-        Submit
-      </button>
+      {cooldown > 0 ? (
+        <button
+          className="px-4 py-2 bg-emerald-700 cursor-disabled text-white font-semibold rounded-md shadow transition cursor-not-allowed"
+        >
+          <LoaderIcon className='animate-spin'/>
+        </button>
+      ) : (
+        <button
+          onClick={onSubmit}
+          className="px-4 py-2 bg-emerald-500 hover:bg-emerald-600 text-white font-semibold rounded-md shadow transition"
+        >
+          <CheckIcon />
+        </button>
+      )}
+
 
       <div className="flex flex-wrap items-center justify-center gap-3 bg-white p-4 rounded-xl shadow-md border border-gray-200">
         {colors.map((color) => (
--- frontend/src/components/UserList.tsx
+++ frontend/src/components/UserList.tsx
@@ -7,17 +7,13 @@ function UserList() {
   return (
     <div className="absolute right-0 top-1/2 -translate-y-1/2 bg-white shadow-lg p-4 rounded-md">
       <h3 className="font-semibold mb-2 text-gray-800">Online Users ({Object.values(presence).length})</h3>
-      <ul className="space-y-1">
+      <div className='flex flex-col'>
         {Object.values(presence).map((user) => (
-          <li key={user.username} className="flex items-center text-sm text-gray-700 gap-2">
-            <span
-              className="inline-block w-2 h-2 rounded-full"
-              style={{ backgroundColor: `#${user.color || "000000"}` }}
-            />
-            {user.username} ({user.x}, {user.y})
-          </li>
+          <div key={user.username} className="flex items-center text-sm text-gray-700 gap-2 border-l-4 m-2" style={{ borderColor: `#${user.color}`}}>
+            <div className='pl-3'>{user.username} ({user.x}, {user.y})</div>
+          </div>
         ))}
-      </ul>
+      </div>
     </div>
   );
 }
